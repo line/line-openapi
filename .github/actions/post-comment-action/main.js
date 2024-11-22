838deda7f7291b64cbf5b3dc54aca391e31a9c4f
Yuta Kasai <kasai.yuta0810@gmail.com>
2024-11-23 00:35:08 +0900

    add comment

--- .github/actions/post-comment-action/main.js
+++ .github/actions/post-comment-action/main.js
@@ -40,9 +40,25 @@ async function run() {
         const url = `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${runId}/job/${jobId}?pr=${prNumber}#step:${stepNumber}:1`;
 
         // Create comment as markdown
-        const fullCommentBody = `### ${language.toUpperCase()} \n` +
+        const fullCommentBody = `## ${language.toUpperCase()} \n` +
             `You can check generated code in ${language}\n\n` +
-            `[Check the diff here](${ url })`;
+            `[Check the diff here](${url})`;
+
+        // check language is one of ruby or php, if so, add comment "You may need to modify code when webhook event is added. Parser is not genareted automatically. Please add test and modify code manually in each repository."
+        // check webhook event is added
+        if (language === 'ruby' || language === 'php') {
+            const { data: { files } } = await octokit.rest.pulls.listFiles({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                pull_number: prNumber,
+            });
+
+            const isWebhookEventAdded = files.some(file => file.filename.includes('webhook.yml'));
+            if (isWebhookEventAdded) {
+                fullCommentBody += `\n\nYou may need to modify code when webhook event is added. Parser is not genareted automatically. Please add test and modify code manually in each repository before release.`;
+            }
+        }
+
 
         // Delete the previous comment if it exists
         const { data: comments } = await octokit.rest.issues.listComments({
